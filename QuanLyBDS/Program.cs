4bd445b4bfc75a738ce30291a1392e48e758d1ad
cdmasd <tienpdps28044@fpt.edu.vn>
2023-11-14 11:58:43 +0700

    UI complete

--- QuanLyBDS/Program.cs
+++ QuanLyBDS/Program.cs
@@ -1,3 +1,4 @@
+using QuanLyBDS.Admin;
 using QuanLyBDS.Guest;
 using QuanLyBDS.KhachHang;
 using QuanLyBDS.NhanVien;
@@ -15,7 +16,7 @@ namespace QuanLyBDS
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new FrmMainNhanVien());
+            Application.Run(new FrmMainGuest());
         }
     }
 }
